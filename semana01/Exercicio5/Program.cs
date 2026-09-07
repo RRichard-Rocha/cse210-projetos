0bb325a382f0528b0030ebeebc4081838dffcaea
Rrichard-Rocha <rrocha2@byupathway.edu>
2026-09-07 18:52:27 -0300

    Resolução Execicio 5

--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        ExibirBoasVindas();
+        string nomeUsuario = NomeDeUsuario();
+        int numeroUsuario = NumeroFavorito();
+        int quadradoUsuario = QuadradoNumero(numeroUsuario);
+        ExibirResultado(nomeUsuario, quadradoUsuario);
+
+        static void ExibirBoasVindas()
+        {
+            Console.WriteLine("Bem-vindo ao programa!");
+        }
+        static string NomeDeUsuario()
+        {
+            Console.Write("Por favor, insira seu nome: ");
+            string entradaUser = Console.ReadLine();
+            return entradaUser;
+        }
+        static int NumeroFavorito()
+        {
+            Console.Write("Por favor, insira seu número favorito:" );
+            int numero = int.Parse(Console.ReadLine());
+            return numero;
+        }
+        static int QuadradoNumero(int numero)
+        {
+            int quadrado = numero * numero;
+            return quadrado;
+        }
+        static void ExibirResultado(string nome, int quadrado)
+        {
+            Console.WriteLine($"{nome}, o quadrado do seu número favorito é {quadrado}");
+        }
+    
     }
 }
