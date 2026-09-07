32f7ec35ccc3589abdf06da9e556b44ac715ea4a
Rrichard-Rocha <rrocha2@byupathway.edu>
2026-09-07 15:08:42 -0300

    Exercicio 3 parte 3

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -5,24 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Qual é o número mágico? ");
-        string entradaUser = Console.ReadLine();
-        int numMagico = int.Parse(entradaUser);
-        Console.Write("Qual é o seu palpite? ");
-        string palpiteUser = Console.ReadLine();
-        int palpite = int.Parse(palpiteUser);
+        Random numAleatorio = new Random();
+        int numMagico = numAleatorio.Next(1,101);
 
-        if (palpite == numMagico)
+        int palpite = -1;
+
+        while (palpite != numMagico)
         {
-            Console.WriteLine("Você Adivinhou");            
-        }
-        else if (palpite < numMagico)
-        {
+            Console.Write("Qual é o seu palpite? ");
+            palpite = int.Parse(Console.ReadLine());
+            
+            if (palpite < numMagico)
+            {
             Console.WriteLine("Mais Alto");
-        }
-        else if ( palpite > numMagico)
-        {
+            }
+            else if ( palpite > numMagico)
+            {
             Console.WriteLine("Mais Baixo");
+            }
+            else
+            {
+                Console.WriteLine("Voce adivinhou!");
+            }
         }
     }
 }
