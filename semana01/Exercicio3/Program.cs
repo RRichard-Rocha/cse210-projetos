4218e8274fed8c12b09c10ef51ec2c2806496bb9
Rrichard-Rocha <rrocha2@byupathway.edu>
2026-09-07 14:37:07 -0300

    Exercicios 1 -3(apenas parte 1e2)

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        Console.Write("Qual é o número mágico? ");
+        string entradaUser = Console.ReadLine();
+        int numMagico = int.Parse(entradaUser);
+        Console.Write("Qual é o seu palpite? ");
+        string palpiteUser = Console.ReadLine();
+        int palpite = int.Parse(palpiteUser);
+
+        if (palpite == numMagico)
+        {
+            Console.WriteLine("Você Adivinhou");            
+        }
+        else if (palpite < numMagico)
+        {
+            Console.WriteLine("Mais Alto");
+        }
+        else if ( palpite > numMagico)
+        {
+            Console.WriteLine("Mais Baixo");
+        }
     }
 }
